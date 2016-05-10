4671e89adab517a7cfacb7e361dca22630aed2ce
Gchmura <gchmura3@gmail.com>
2016-05-10 14:49:57 +0200

    Update z1p2.cpp

--- z1p2/z1p2.cpp
+++ z1p2/z1p2.cpp
@@ -9,7 +9,7 @@
 #endif
 
 ///////////////////////////////////////////////////////////
-// Wywo≥ywana w celu przerysowania sceny
+// Wywo≈Çywana w celu przerysowania sceny
 
 
 void RenderScene(void) {
@@ -18,7 +18,7 @@ void RenderScene(void) {
 	// Aktualny kolor rysujcy - pomaranczowy 
 	//           R     G     B  
 	glColor3f(.98f, 0.625f, 0.12f);
-	// Narysowanie 14 kata wype≥nionego aktualnym kolorem  
+	// Narysowanie  6 kata wype≈Çnionego aktualnym kolorem  
 
 	glBegin(GL_POLYGON);
 	glVertex2f(0.0f, 60.0f);
@@ -29,7 +29,7 @@ void RenderScene(void) {
 	glVertex2f(-50.0f, 30.0f);
 
 	glEnd();
-	// Wys≥anie polece do wykonania     
+	// Wys≈Çanie polece do wykonania     
 	glFlush();
 
 
@@ -42,24 +42,24 @@ void SetupRC(void) {
 	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 }
 ///////////////////////////////////////////////////////////
-// Wywo≥ywana przez bibliotek GLUT w przypadku zmiany rozmiaru okna
+// Wywo≈Çywana przez bibliotek GLUT w przypadku zmiany rozmiaru okna
 void ChangeSize(int w, int h) {
 	GLfloat aspectRatio;
 	// Zabezpieczenie przed dzieleniem przez zero  
 	if (h == 0)   h = 1;
-	// Ustawienie wielkoúci widoku na rÛwnπ wielkoúci okna     
+	// Ustawienie wielko≈õci widoku na r√≥wnƒÖ wielko≈õci okna     
 	glViewport(0, 0, w, h);
-	// Ustalenie uk≥adu wspÛ≥rzÍdnych  
+	// Ustalenie uk≈Çadu wsp√≥≈Çrzƒôdnych  
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	// Wyznaczenie przestrzeni ograniczajπcej (lewy, prawy, dolny, gÛrny, bliski, odleg≥y)  
+	// Wyznaczenie przestrzeni ograniczajƒÖcej (lewy, prawy, dolny, g√≥rny, bliski, odleg≈Çy)  
 	aspectRatio = (GLfloat)w / (GLfloat)h;
 	if (w <= h)    glOrtho(-200.0, 200.0, -200 / aspectRatio, 200.0 / aspectRatio, 2.0, -2.0);
 	else    glOrtho(-200.0 * aspectRatio, 200.0 * aspectRatio, -200.0, 200.0, 2.0, -2.0);
 	glMatrixMode(GL_MODELVIEW);  glLoadIdentity();
 }
 ///////////////////////////////////////////////////////////
-// G≥Ûwny punkt wejcia programu
+// G≈Ç√≥wny punkt wejcia programu
 int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
